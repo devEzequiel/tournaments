0161a4e5aa02c1ae4679b3f25ed7f3a1d5547eb1
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-01 13:39:36 -0300

    Update template.php

--- views/template.php
+++ views/template.php
@@ -3,13 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?> assets/css/style.css">
 	<title>Tournaments</title>
 </head>
 <body>
-	<h1>Torneios</h1>
-	<a href="<?php echo BASE_URL; ?>">Torneios</a>
-	<a href="<?php echo BASE_URL; ?>galeria">Galeria</a>
-
+	<div class="container">
+	  <nav class="navbar navbar-expand-lg navbar-light" style= "background-color: #2F8DC2;">
+	    <a class="navbar-brand" href="">Navbar</a>
+	  </nav>
+	</div>
 	<?php echo $this->loadViewInTemplate($viewName, $viewData);?>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 </html>
