ca32a3fc91d953ed47409642adcd66917f551cf6
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-03 11:56:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,15 @@ require 'config.php';
 spl_autoload_register(function($class){
 
 	if(file_exists('controllers/'.$class.'.php')) {
+
 		require 'controllers/'.$class.'.php';
 
 	} else if (file_exists('models/'.$class.'.php')) {
+
 		require 'models/'.$class.'.php';
 
 	} else if (file_exists('core/'.$class.'.php')) {
+		
 		require 'core/'.$class.'.php';
 
 	}
