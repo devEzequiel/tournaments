8cd4500abd3ab13b63d37ce5c162e62bb7a48449
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-03 11:12:26 -0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,2 +1,41 @@
+<div class="container torneio">
 
+	<div>
+		<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Matches</th>
+      <th scope="col">Gols</th>
+      <th scope="col">Assists</th>
+      <th scope="col">Avrg</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Kliment</th>
+      <td>4</td>
+      <td>7</td>
+      <td>3</td>
+      <td>6.9</td>
+    </tr>
+    <tr>
+      <th scope="row">Diego</th>
+      <td>7</td>
+      <td>2</td>
+      <td>5</td>
+      <td>7.3</td>
+    </tr>
+    <tr>
+      <th scope="row">Lenny</th>
+      <td>7</td>
+      <td>2</td>
+      <td>5</td>
+      <td>7.3</td>
+    </tr>
+  </tbody>
+</table>
+	</div>
+
+</div>
 
