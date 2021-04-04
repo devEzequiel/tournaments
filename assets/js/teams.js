b277ab05796fdfa69958511a780dedc5ac985bc7
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-03 23:30:06 -0300

    Create teams.js

---
+++ assets/js/teams.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+// 	$('#form-team').on('submit', function(e){
+// 		e.preventDefault();
+// 		let nome = $('#input-team').val();
+// 		$.ajax({
+// 			data:{
+// 					name: nome,
+// 					new: '000'
+// 				},
+// 			datatype: 'json',
+// 			type: 'get',
+// 			url: 'http://localhost/GitHub/tournaments/models/Teams.php' 
+// 		}).done(function(result){
+// 			console.log(+result);
+// 		})
+// 	})
+// })
