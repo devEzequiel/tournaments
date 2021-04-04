04fe39f3913dc4a476418f0cd004cafbc2ea35e1
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-04 17:38:59 -0300

    Update TeamsController.php

--- controllers/TeamsController.php
+++ controllers/TeamsController.php
@@ -8,4 +8,14 @@ class teamsController extends Controller
 	{
 		$this->loadTemplate('teams');
 	}
+
+	public function adicionar ()
+	{
+		$teams = new Teams();
+		$teams->setTeamName($_GET['name']);
+		if ($teams) {
+        	// header ('Content-type: application/json');
+        	var_dump (array(0 => 'ok'));
+        }
+	}
 }
