de62e48434bc03ae46f484f607d9d28162bb15aa
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-05 15:39:44 -0300

    Update teams.js

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -1,16 +1,21 @@
-// $(document).ready(function(){
-// 	$('#form-team').on('submit', function(e){
-// 		e.preventDefault();
-// 		let nome = $('#input-team').val();
-// 		$.ajax({
-// 			data:{
-// 					name: nome
-// 				},
-// 			datatype: 'json',
-// 			type: 'get',
-// 			url: 'http://localhost/GitHub/tournaments/teams/adicionar' 
-// 		}).done(function(result){
-// 			console.log(+result);
-// 		})
-// 	})
-// })
+$(document).ready(function(){
+	$('#form-team').on('submit', function(e){
+		e.preventDefault();
+		let nome = $('#name-team').val();
+		$.ajax({
+			data:{
+					name: nome
+				},
+			datatype: 'json',
+			type: 'get',
+			url: 'http://localhost/GitHub/tournaments/teams/adicionar' 
+		}).done(function(result){
+			
+			if (result == 0) {
+				$('#team-msg').html('<div class="alert alert-success" id="msg" role="alert">Time adicionado com Sucesso! Recarregue a página.</div>');
+			} else {
+				$('#team-msg').html('<div class="alert alert-danger" id="msg" role="alert">Esse time já existe</div>')
+			}
+		})
+	})
+})
