3e6541deda8647c94efe17e58a07fcc7114da8c6
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-05 18:58:29 -0300

    Create players.js

---
+++ assets/js/players.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('#form-player').on('submit', function(e){
+		e.preventDefault();
+		let name = $('#formp').val();
+		let gols = $('#formp0').val();
+		let ass = $('#formp1').val();
+		let matchez = $('#formp2').val();
+		let time = $('#team_namm').val();
+		let media = $('#formp3').val();
+		$.ajax({
+			data:{
+					name: nome,
+					goals: gols,
+					assists: ass,
+					matches: matchez,
+					team: time,
+					avrg: media
+				},
+			datatype: 'json',
+			type: 'get',
+			url: 'http://localhost/GitHub/tournaments/teams/adicionar' 
+		}).done(function(result){
+			
+			if (result == 0) {
+				$('#team-msg').html('<div class="alert alert-success" id="msg" role="alert">Jogador adicionado com Sucesso! Recarregue a página.</div>');
+			} else {
+				$('#team-msg').html('<div class="alert alert-danger" id="msg" role="alert">Esse jogador já existe</div>')
+			}
+		})
+	})
+})
