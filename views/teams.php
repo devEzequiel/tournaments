7fff59cd6429757738d3c3a73413f9b3dfea35e0
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-05 15:40:13 -0300

    Update teams.php

--- views/teams.php
+++ views/teams.php
@@ -1,15 +1,16 @@
 
 
 <!-- conteudo da pagina-->
-<h3 class="" id="teams-tt">Times</h3>
 <div class="list-group" id="teams">
-	<a href="#" class="novo-time" id="novoteam" data-toggle="modal" data-target="#modalteam">Criar Novo Time</a>
-	<a href="#" class="list-group-item list-group-item-action team-link">Cras justo odio</a>
-    <a href="#" class="list-group-item list-group-item-action team-link">Dapibus ac facilisis in</a>
-	<a href="#" class="list-group-item list-group-item-action team-link">Morbi leo risus</a>
-  	<a href="#" class="list-group-item list-group-item-action team-link">Porta ac consectetur ac</a>
-  	<a href="#" class="list-group-item list-group-item-action team-link">Vestibulum at eros</a>
+	<a href="#" class="novo-time" id="novoteam" data-toggle="modal" data-target="#modalteam">NOVO TIME</a>
+
+  <?php foreach ($viewData['team'] as $t) { ?>
+    
+  	<a href="<?php echo BASE_URL; ?>teams/abrir/<?php echo $t['team_id'];?>" class="list-group-item list-group-item-action team-link"><?php echo $t['team_name']?> </a>
+  
+  <?php } ?>
 </div>
+
 
 <!-- Modal Times -->
 <div class="modal" tabindex="-1" role="dialog" id="modalteam">
@@ -25,8 +26,10 @@
      		<form id="form-team" method="GET" action="<?php BASE_URL; ?>teams/adicionar">
     		  <div class="form-group">
     		    <label for="exampleInputEmail1">Nome do time</label>
-    		    <input name="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="Nome do Time:">
+    		    <input name="name" type="text" class="form-control" id="name-team" placeholder="Nome do Time:">
+            <div id="team-msg"></div>
             <div class="modal-footer">
+        
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
               <input type="submit" class="btn" id="input-team" value="Adicionar">
             </div>
