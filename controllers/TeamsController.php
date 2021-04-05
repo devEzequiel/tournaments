493ab6796bcc40fc3e8e313c751815d3eb96c790
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-05 15:39:53 -0300

    Update TeamsController.php

--- controllers/TeamsController.php
+++ controllers/TeamsController.php
@@ -1,21 +1,53 @@
 <?php
-/**
- * 
- */
+
+
+
 class teamsController extends Controller
 {
 	public function index ()
 	{
-		$this->loadTemplate('teams');
+		$teams = new Teams ();
+		$data = [];
+		$data['team'] = $teams->getTeams();
+		$this->loadTemplate('teams', $data);
 	}
 
 	public function adicionar ()
 	{
 		$teams = new Teams();
-		$teams->setTeamName($_GET['name']);
-		if ($teams) {
-        	// header ('Content-type: application/json');
-        	var_dump (array(0 => 'ok'));
-        }
+		
+		switch ($teams->setTeamName($_GET['name'])) {
+			case 0:	
+				header ('Content-type: application/json');			
+        		echo json_encode(array(0 => 0));
+				break;			
+			case 1:	
+				header ('Content-type: application/json');			
+        		echo json_encode(array(0 => 1));
+				break;
+			case 2:	
+				header ('Content-type: application/json');			
+	    		echo json_encode(array(0 => 2));
+				break;
+			default:
+				header ('Content-type: application/json');
+				echo json_encode(array(0 => 3));
+				break;
+		}
+	}
+
+	public function abrir ($team_id)
+	{
+		$players = new Players ();
+		$team = new Teams ();
+
+		$data = [];
+
+		$data['stats'] = $players->getStats($team_id);
+
+		$data['team'] = $team->getTeamName($team_id);
+
+		$this->loadTemplate('abrir', $data);
+
 	}
 }
