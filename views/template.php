2c1060b8811af74e55fbf4c9f486584b68575dcd
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-05 15:40:16 -0300

    Update template.php

--- views/template.php
+++ views/template.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
 
-	<link rel="stylesheet" type="text/css" href="<?php BASE_URL;?>assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css">
 	<title>Tournaments</title>
 </head>
 <body>
