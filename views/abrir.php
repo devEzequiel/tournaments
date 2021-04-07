341d80f7ff45f5f40f93ebc0abc22487f251336f
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-06 22:23:53 -0300

    Update abrir.php

--- views/abrir.php
+++ views/abrir.php
@@ -6,30 +6,30 @@
 </div>
 <div class="container torneio">
 	<div>
-		<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Matches</th>
-      <th scope="col">Gols</th>
-      <th scope="col">Assists</th>
-      <th scope="col">Média</th>
-    </tr>
-  </thead>
+		<table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Matches</th>
+          <th scope="col">Gols</th>
+          <th scope="col">Assists</th>
+          <th scope="col">Média</th>
+        </tr>
+      </thead>
 
-  <?php foreach ($viewData['stats'] as $p) {?>
-  <tbody>
-    <tr>
-      <th scope="row"><?php echo $p['name']; ?></th>
-      <td><?php echo $p['matches']; ?></td>
-      <td><?php echo $p['goals']; ?></td>
-      <td><?php echo $p['assists']; ?></td>
-      <td><?php echo $p['average']; ?></td>
-    </tr>
-  <?php } ?>
-    
-  </tbody>
-</table>
+      <?php foreach ($viewData['stats'] as $p) {?>
+      <tbody>
+        <tr>
+          <th scope="row"><?php echo $p['name']; ?></th>
+          <td><?php echo $p['matches']; ?></td>
+          <td><?php echo $p['goals']; ?></td>
+          <td><?php echo $p['assists']; ?></td>
+          <td><?php echo $p['average']; ?></td>
+        </tr>
+      <?php } ?>
+        
+      </tbody>
+    </table>
 	</div>
 
 </div>
@@ -45,26 +45,26 @@
         </button>
       </div>
       <div class="modal-body">
-        <form id="form-player" method="GET" action="<?php echo BASE_URL; ?>players/adicionar">
+        <form id="form-player">
           <div class="form-group">
             <label for="formp" style="margin-right: 115px;">Nome do Jogador</label>
             <label for="formp0">Gols Feitos</label>
             <div class="row">
-              <input name="name" type="text" class="form-control col" id="formp" placeholder="Nome do Jogador:">
-              <input name="goals" value="0" type="text" class="form-control col" id="formp0" placeholder="Quantidade de Gols">
+              <input name="name" type="text" class="form-control col" id="formp" placeholder="Nome do Jogador:" required>
+              <input name="goals" value="0" type="text" class="form-control col" id="formp0" placeholder="Quantidade de Gols" required>
             </div>
             <label for="formp1" style="margin-right: 115px;">Nº de Assistências</label>
             <label for="formp2">Nº de Partidas Jogadas</label>
             <div class="row">
-              <input name="assists" value="0" type="text" class="form-control col" id="formp1" placeholder="Assistências">
-              <input name="matches" value="0" type="text" class="form-control col" id="formp2" placeholder="Partidas Jogadas">
+              <input name="assists" value="0" type="text" class="form-control col" id="formp1" placeholder="Assistências" required>
+              <input name="matches" value="0" type="text" class="form-control col" id="formp2" placeholder="Partidas Jogadas" required>
             </div>
             <label for="formp" style="margin-right: 162px;">Nota Média</label>
             <label for="team_namm">Nome do Time</label>
             
             <div class="row">
-              <input name="avrg" value="0.0" type="text" class="form-control col" id="formp3" placeholder="Nota Média">
-              <input name="team" value="<?php echo $viewData['team']['team_name']; ?>" type="text" class="form-control col" id="team_namm" readonly>
+              <input name="avrg" value="0.0" type="text" class="form-control col" id="formp3" placeholder="Nota Média" required>
+              <input name="team" value="<?php echo $viewData['team']['team_name']; ?>" type="text" class="form-control col" id="team_namm" readonly required>
             </div>          
             
             <div id="team-msg"></div>
