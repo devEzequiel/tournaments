4b62614ca93a48a22b989503fb0504de405fde7c
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 14:15:17 -0300

    Update match.php

--- views/match.php
+++ views/match.php
@@ -1,32 +1,62 @@
 <div class="container">
-	<form>
-		<div class="row" id="selects">
-			<div class="" id="div1">
-				<select class="sel select-team" id="sel1" style="float:left;">
-					<option value="" selected disabled>Selecione um time ...</option>
+	<div class="row" id="selects">
+		<div class="" id="div1">
+			<select class="sel select-team" id="sel1" style="float:left;">
+				<option value="" selected disabled>Selecione um time ...</option>
 
-					<?php foreach ($viewData as $team) {
-					 ?>
-					<option value="<?php echo $team['team_name']; ?>"><?php echo $team['team_name']; ?></option>
-					<?php } ?>
-				</select> 
-			</div>
-
-			<div class="" id="xis">
-				<p>X</p>
-			</div>
-
-			<div class="" id="div2">
-				<select id="sel2" class="sel select-team" >
-					<option value="" selected disabled> Selecione um time ...</option>
-
-					 <?php foreach ($viewData as $team) {
-					 ?>
-					<option value="<?php echo $team['team_name']; ?>"><?php echo $team['team_name']; ?></option>
-					<?php } ?>
-				</select> 
-			</div>
-			
+				<?php foreach ($viewData as $team) {
+				 ?>
+				<option value="<?php echo $team['team_name']; ?>"><?php echo $team['team_name']; ?></option>
+				<?php } ?>
+			</select> 
 		</div>
+
+		<div class="" id="xis">
+			<p>X</p>
+		</div>
+
+		<div class="" id="div2">
+			<select id="sel2" class="sel select-team" >
+				<option value="" selected disabled> Selecione um time ...</option>
+
+				 <?php foreach ($viewData as $team) {
+				 ?>
+				<option value="<?php echo $team['team_name']; ?>"><?php echo $team['team_name']; ?></option>
+				<?php } ?>
+			</select> 
+		</div>
+		
+	</div>
+<div class="row">
+	<form id="form1">
+		<table class="table" id="table1" style="width: 460px; margin-top: 60px;">
+		  <thead>
+		    <tr>
+		      <th scope="col">Name</th>
+		      <th scope="col">Rate</th>
+		      <th scope="col">Goals</th>
+		      <th scope="col">Assists</th>
+		    </tr>
+		  </thead>
+		  <tbody id="tables1">
+
+		  </tbody>
+		</table>
 	</form>
+	<form id="form2">
+		<table class="table" id="table2" style="width: 460px; margin-top: 60px; margin-left: 100px;">
+		  <thead>
+		    <tr>
+		      <th scope="col">Name</th>
+		      <th scope="col">Rate</th>
+		      <th scope="col">Goals</th>
+		      <th scope="col">Assists</th>
+		    </tr>
+		  </thead>
+		  <tbody id="tables2">
+		    
+		  </tbody>
+		</table>
+	</form>
+</div>
 </div>
