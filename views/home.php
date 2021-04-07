1a86b1b1f401f50f17f4822e4c2551ccf17bee25
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 15:12:31 -0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,3 +1,6 @@
+<?php
+  //error_reporting(1);
+?>
 <div class="container torneio">
 
 	<div>
@@ -6,10 +9,10 @@
     <tr>
       <th scope="col">Nome</th>
       <th scope="col" style="">Team</th>
-      <th scope="col" style="cursor: pointer;">Matches</th>
+      <th scope="col" style="cursor: pointer;">Partidas</th>
       <th scope="col" style="cursor: pointer;">Gols</th>
       <th scope="col" style="cursor: pointer;">Assists</th>
-      <th scope="col" style="cursor: pointer;">Avrg</th>
+      <th scope="col" style="cursor: pointer;">Média</th>
     </tr>
   </thead>
   <?php
@@ -18,7 +21,36 @@
   <tbody>
     <tr>
       <th scope="row"><?php echo $p['name']; ?></th>
-      <td><?php echo $p['matches']; ?></td>
+      <?php switch ($p['team_id']) {
+        case '4':
+          $team = 'Atalanta';
+          break;
+
+        case '7':
+          $team = 'Bordeaux';
+          break;
+
+        case '9':
+          $team = 'Angers';
+          break;
+
+        case '10':
+          $team = 'Napoli';
+          break;
+
+        case '11':
+          $team = 'Roma';
+          break;
+
+        case '12':
+          $team = 'Lille';
+          break;
+        
+        default:
+          $team = 'Free Agent';
+          break;
+      } ?>
+      <td><?php echo $team; ?></td>
       <td><?php echo $p['matches']; ?></td>
       <td><?php echo $p['goals']; ?></td>
       <td><?php echo $p['assists']; ?></td>
