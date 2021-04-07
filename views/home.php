73897426d4c916207c23ff001739163b4e070855
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 14:15:13 -0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -5,6 +5,7 @@
   <thead class="thead-dark">
     <tr>
       <th scope="col">Nome</th>
+      <th scope="col" style="">Team</th>
       <th scope="col" style="cursor: pointer;">Matches</th>
       <th scope="col" style="cursor: pointer;">Gols</th>
       <th scope="col" style="cursor: pointer;">Assists</th>
@@ -17,6 +18,7 @@
   <tbody>
     <tr>
       <th scope="row"><?php echo $p['name']; ?></th>
+      <td><?php echo $p['matches']; ?></td>
       <td><?php echo $p['matches']; ?></td>
       <td><?php echo $p['goals']; ?></td>
       <td><?php echo $p['assists']; ?></td>
