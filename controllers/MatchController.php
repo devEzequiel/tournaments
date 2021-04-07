a0c22cdfba810746dc5991a5e92bf45b6c2b3292
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 14:15:07 -0300

    Update MatchController.php

--- controllers/MatchController.php
+++ controllers/MatchController.php
@@ -13,4 +13,16 @@ class matchController extends Controller
 		$data = $teams->getTeams();
 		$this->loadTemplate('match', $data);
 	}
+
+	public function showtable()
+	{	
+		$players = new Players();
+		$players->setTeamName($_GET['team']);
+		$data = $players->getPlayersName();
+
+		header ('Content-type: application/json');			
+		echo json_encode($data);
+
+	}
 }
+
