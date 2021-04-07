efa8ad2062cb2bb9c3e6e7df152ae7dca9093938
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 15:12:25 -0300

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -6,8 +6,8 @@ class homeController extends Controller
 	public function index()
 	{
 		$players = new Players();
+		$teams = new Teams;
 		$data = $players->getPlayersByGoals();
-
 		$this->loadTemplate('home', $data);
 	}
 
