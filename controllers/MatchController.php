fc2d26dbe3ada3863daa97362d906a2f8a97128a
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-07 23:15:02 -0300

    Update MatchController.php

--- controllers/MatchController.php
+++ controllers/MatchController.php
@@ -24,5 +24,36 @@ class matchController extends Controller
 		echo json_encode($data);
 
 	}
+
+	public function addstats()
+	{
+		$p = new Players;
+
+		$name = [];
+		foreach ($_POST as $key => $value) {
+			$name[] = substr($key, 4);
+		}
+		
+		$player = [];
+		for ($x=0 ; $x < count($name) ; $x += 3 ) { 
+			$player[] = $name[$x];
+		}
+		$y = 0;
+		for ($x=0; $x < count($player); $x++) { 
+
+			$p->setName($player[$x]);
+			$p->setGoals($_POST['goal'.$player[$x]]);
+			$p->setAssists($_POST['assi'.$player[$x]]);
+			$p->setAverage($_POST['avrg'.$player[$x]]);
+
+			$p->setMatch();
+
+			$y++;
+		}
+
+		if ($y == count($player)){
+			header("Location: ".BASE_URL."match?insert=ok");
+		}
+	}
 }
 
