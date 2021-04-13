dc645374ae7415e61a4fafc8fa6b55d2d44b1433
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-13 12:39:31 -0300

    Update players.js

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,3 +1,79 @@
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ){
+    if ( ns.includes("noPreventDefault") ) {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } else {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
+
+//json and table population
+function jsonHandle(json){
+	let html = '';
+	let count = json.length;
+	let x = 0;
+
+	for (var i = 0; i < count; i++) {
+
+		switch(json[i]['team_id']){
+
+			case '4':
+				var team = 'Atalanta';
+				break;
+			case '7':
+				var team = 'Bordeaux';
+				break;
+			case '9':
+				var team = 'Angers';
+				break;
+			case '10':
+				var team = 'Napoli';
+				break;
+			case '11':
+				var team = 'Roma';
+				break;
+			case '12':
+				var team = 'Lille';
+				break;
+			
+		}
+
+		switch(x){
+			case 0:
+				var id = 'tbf';
+				break;
+			case 1:
+				var id = 'tbs';
+				break;
+			case 2:
+				var id = 'tbt';
+				break;
+			default:
+				id = '';
+		}
+
+		if(x<3){
+			html += '<tbody id="stats"><tr id="'+id+'"><td scope="row">'+json[i]['name']+'</td><td>'+team+'</td><td>'+json[i]['matches']+'</td><td>'+json[i]['goals']+'</td><td>'+json[i]['assists']+'</td><td>';
+			x++;
+		} else {
+			html += '<tbody><tr id=""><td scope="row">'+json[i]['name']+'</td><td>'+team+'</td><td>'+json[i]['matches']+'</td><td>'+json[i]['goals']+'</td><td>'+json[i]['assists']+'</td><td>';
+		}
+
+		if (json[i]['matches'] != 0) {
+			html += json[i]['score']/json[i]['matches'];
+		} else {
+			html += 0;
+		}
+
+		html+= '</td></tr></tbody>'
+		
+	}
+
+	$('#stats').replaceWith(html);
+}
+
+//add new player
 $(document).ready(function(){
 	$('#form-player').on('submit', function(e){
 		e.preventDefault();
@@ -30,3 +106,70 @@ $(document).ready(function(){
 		})
 	})
 })
+//get all players from database and screens in a table
+$(document).ready(function(){
+	$.ajax({
+		datatype: 'json',
+		url: './home/stats',
+		type: 'get'
+	}).done(function(json){
+		jsonHandle(json);
+	})
+})
+
+//get all players ordered by goals when goal th has clicked
+$(document).ready(function(){
+	$('#b-goal').on('click', function(){
+		$.ajax({
+			datatype: 'json',
+			url: './home/stats',
+			type: 'get'
+		}).done(function(json){
+			jsonHandle(json);
+		})
+
+	})
+})
+
+
+//get all players by assists when click in the assists th
+$(document).ready(function(){
+	$('#b-assist').on('click', function(){
+		$.ajax({
+			datatype: 'json',
+			url: './home/byAssists',
+			type: 'get'
+		}).done(function(json){
+			jsonHandle(json);
+		})
+
+	})
+})
+
+//get all players by average when click in the Media th
+$(document).ready(function(){
+	$('#b-avrg').on('click', function(){
+		$.ajax({
+			datatype: 'json',
+			url: './home/byScore',
+			type: 'get'
+		}).done(function(json){
+			jsonHandle(json);
+		})
+
+	})
+})
+
+//get all players by matches number when click in the Partidas th
+$(document).ready(function(){
+	$('#b-match').on('click', function(){
+		$.ajax({
+			datatype: 'json',
+			url: './home/byMatches',
+			type: 'get'
+		}).done(function(json){
+			jsonHandle(json);
+		})
+	})
+})
+
