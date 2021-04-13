fc5c8b581afa17eb45960b6d71a8bc17fa520d2c
Ezequiel Oliveira <esofinney@gmail.com>
2021-04-13 12:39:41 -0300

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -8,7 +8,39 @@ class homeController extends Controller
 		$players = new Players();
 		$teams = new Teams;
 		$data = $players->getPlayersByGoals();
+
 		$this->loadTemplate('home', $data);
 	}
 
+	public function stats(){
+		$players = new Players();
+		$data = $players->getPlayersByGoals();
+
+		header ('Content-type: application/json');
+		echo json_encode($data);
+	}
+
+	public function byAssists(){
+		$players = new Players();
+		$data = $players->getPlayersByAssists();
+
+		header ('Content-type: application/json');
+		echo json_encode($data);
+	}
+
+	public function byScore(){
+		$players = new Players();
+		$data = $players->getPlayersByScore();
+
+		header ('Content-type: application/json');
+		echo json_encode($data);
+	}
+
+	public function byMatches(){
+		$players = new Players();
+		$data = $players->getPlayersByMatches();
+
+		header ('Content-type: application/json');
+		echo json_encode($data);
+	}
 }
